7fa884f09977b3bbef5fd6338b936f334e91be40
shzkhan1111 <91877371+shzkhan1111@users.noreply.github.com>
2022-01-03 18:47:19 +0500

    Inheriting Components

---
+++ Blazor Server assembly/BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Server_assembly.BaseComponents
+{
+
+    //make it inherit form ComponentBase
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+    }
+}
